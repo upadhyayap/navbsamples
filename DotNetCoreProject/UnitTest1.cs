84c128b5712b7cd62343c6de37fb8ef30cd56a2a
Navin <navin22@github.com>
2021-04-30 16:21:37 +0530

    Dotnet core test changes

--- DotNetCoreProject/UnitTest1.cs
+++ DotNetCoreProject/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DotNetCoreProject
@@ -5,6 +6,21 @@ namespace DotNetCoreProject
     [TestClass]
     public class UnitTest1
     {
+        public TestContext TestContext { get; set; }
+
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Console.WriteLine("ClassInitialize");
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            Console.WriteLine("ClassCleanup");
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
@@ -23,7 +39,17 @@ namespace DotNetCoreProject
         [TestMethod]
         public void TestMethod1111()
         {
+            TestContext.WriteLine("This is a filing test");
             Assert.AreEqual(1, 0);
+        }
+
+        [TestMethod]
+        [DataRow(1, 2, 3)]
+        [DataRow(2, 2, 4)]
+        [DataRow(2, 3, 4)]
+        public void TestMehtod4(int a, int b, int c)
+        {
+            Assert.AreEqual(a + b, c);  
         }
     }
 }
